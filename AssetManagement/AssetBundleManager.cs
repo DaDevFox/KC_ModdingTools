445397a0a6f332bd441e093cc513aafbd05d8871
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2020-04-24 20:09:28 -0400

    Update AssetBundleManager.cs

--- AssetManagement/AssetBundleManager.cs
+++ AssetManagement/AssetBundleManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,43 +11,14 @@ namespace KCModUtils.AssetManagement
     {
         private static AssetBundle assetBundle;
         /// <summary>
-        /// The path of the AssetBundle relative to the root of the mod folder
-        /// </summary>
-        public static string AssetBundleRelativePath = "/assetbundle/";
-        /// <summary>
-        /// The name of the assetbundle as defined in the Unity Editor. 
-        /// </summary>
-        public static string AssetBundleName = "statisticsmodassets";
-
-        static KCModHelper helper;
-
-        /// <summary>
-        /// Set this to recieve output messages if there were issues loading the AssetBundle. 
-        /// </summary>
-        public static KCModHelper Helper
-        {
-            get
-            {
-                return helper;
-            }
-            set
-            {
-                helper = value;
-                hasHelper = helper != null;
-            }
-        }
-
-        private static bool hasHelper = false;
-
-        /// <summary>
         /// Loads the AssetBundle in for the first time, must be called before attempting to get an asset. 
         /// </summary>
         public static void UnpackAssetBundle()
         {
-            assetBundle = KCModHelper.LoadAssetBundle(helper.modPath + AssetBundleRelativePath, AssetBundleName);
-            
-            if (assetBundle == null && hasHelper) 
-                helper.Log("AssetBundle Failed to load");
+            assetBundle = KCModHelper.LoadAssetBundle(ModUtilsMain.Helper.modPath + ModUtilsMain.AssetBundleRelativePath, ModUtilsMain.AssetBundleName);
+
+            if (assetBundle == null)
+                ModUtilsMain.Helper.Log("AssetBundle Failed to load");
         }
 
 
@@ -56,7 +27,7 @@ namespace KCModUtils.AssetManagement
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
-        public static object GetAssetByPath(string path) 
+        public static object GetAssetByPath(string path)
         {
             return assetBundle.LoadAsset(path);
         }
@@ -66,7 +37,7 @@ namespace KCModUtils.AssetManagement
         /// </summary>
         /// <param name="name">The name of the asset to look for</param>
         /// <returns>the first asset in the assetbundle that is loaded with the given name</returns>
-        public static object GetAsset(string name) 
+        public static object GetAsset(string name)
         {
             if (assetBundle.Contains(name))
             {
@@ -76,16 +47,16 @@ namespace KCModUtils.AssetManagement
                 {
                     string[] pathParts = paths[i].Split('/');
                     string assetName = pathParts[pathParts.Length - 1];
-                    if(assetName == name.ToLower())
+                    if (assetName == name.ToLower())
                     {
                         Asset = assetBundle.LoadAsset(paths[i]);
                     }
                 }
                 return Asset;
             }
-            else {
-                if(hasHelper)
-                    helper.Log("Asset not found: " + name);
+            else
+            {
+                ModUtilsMain.Helper.Log("Asset not found: " + name);
                 return null;
             }
         }
