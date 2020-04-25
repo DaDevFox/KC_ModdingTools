c47a1896631f9eef301ff24438984521f6f5fadd
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2020-04-24 20:09:10 -0400

    Update DebugExt.cs

--- Debugging/DebugExt.cs
+++ Debugging/DebugExt.cs
@@ -1,55 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 
 namespace KCModUtils.Debugging
 {
     class DebugExt
     {
-        
-
         private static List<int> IDs = new List<int>();
-
-        public static string modID = "someModId";
-        public static bool Debug;
-
-        static KCModHelper helper;
-
-        /// <summary>
-        /// Set this to recieve output messages if there were issues loading the AssetBundle. 
-        /// </summary>
-        public static KCModHelper Helper
-        {
-            get
-            {
-                return helper;
-            }
-            set
-            {
-                helper = value;
-                hasHelper = helper != null;
-            }
-        }
-
-        private static bool hasHelper = false;
 
         public static void Log(string message, bool repeatable = false, KingdomLog.LogStatus type = KingdomLog.LogStatus.Neutral, object GameObjectOrVector3 = null)
         {
-            KingdomLog.TryLog(modID + "_debugmsg-" + IDs.Count + (repeatable ? SRand.Range(0, 1).ToString() : ""), message, type, (repeatable ? 1 : 20), GameObjectOrVector3);
-            IDs.Add(1);
+            if (ModUtilsMain.debug)
+            {
+                KingdomLog.TryLog(ModUtilsMain.ModID + "_debugmsg-" + IDs.Count + (repeatable ? SRand.Range(0, 1).ToString() : ""), message, type, (repeatable ? 1 : 20), GameObjectOrVector3);
+                IDs.Add(1);
+            }
         }
 
         public static void HandleException(Exception ex)
         {
-            if (Debug)
+            if (ModUtilsMain.debug)
                 DebugExt.Log(ex.Message + "\n" + ex.StackTrace);
             else
-                helper.Log(ex.Message + "\n" + ex.StackTrace);
+                ModUtilsMain.Helper.Log(ex.Message + "\n" + ex.StackTrace);
         }
-
-
     }
 }
